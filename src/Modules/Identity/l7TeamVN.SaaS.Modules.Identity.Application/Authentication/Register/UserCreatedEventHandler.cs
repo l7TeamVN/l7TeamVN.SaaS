cdf13e3850910a1597435e9f96d7bccad07b573e
Nguyen Xuan Dung <l7ungdzk4@gmail.com>
2026-04-23 17:51:48 +0700

    Fix handler domain event

---
+++ src/Modules/Identity/l7TeamVN.SaaS.Modules.Identity.Application/Authentication/Register/UserCreatedEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using l7TeamVN.SaaS.Domain.Events;
+using l7TeamVN.SaaS.Modules.Identity.Domain.Events;
+
+namespace l7TeamVN.SaaS.Modules.Identity.Application.Authentication.Register;
+
+public class UserCreatedEventHandler : IDomainEventHandler<UserCreatedEvent>
+{
+    public async Task Handle(UserCreatedEvent notification, CancellationToken cancellationToken)
+    {
+    }
+}
